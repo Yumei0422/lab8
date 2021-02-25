90e4f06ea3f82da2381f06737193bad5c0370981
Yumei Feng <y9feng@ucsd.edu>
2021-02-25 15:52:10 +0800

    thirdpartyAPI lab

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,15 @@
 function initMap() {
 	// add your code here
+	L.mapquest.key = 'LL8GWZtL3AjUU2QUKhCvuqUBF0AYW8O1';
+
+	// 'map' refers to a <div> element with the ID map
+	var map = L.mapquest.map('map', {
+  		center: [32.8789272, -117.2370064],
+  		layers: L.mapquest.tileLayer('map'),
+  		zoom: 12,
+  		zoomControl: false
+	});
+
+	//Add marker to the map
+	L.marker([32.88,-117.236]).addTo(map);
 }
--- routes/rsvp.js
+++ routes/rsvp.js
@@ -1,10 +1,17 @@
 var data = {
-  rsvp: ['ixd@ucsd.edu']
+	rsvp: ['ixd@ucsd.edu']
 };
 
-/*
- * GET tasks page.
- */
 exports.adminView = function(request, response){
-  response.render('rsvp', data);
+	response.render('rsvp', data);
 };
+
+exports.addRSVP = function(request, response) {
+	var rsvpEmail = request.body.rsvpEmail;
+	//Print to terminal whenever the post request is 
+	console.log(rsvpEmail);
+    //add to current data
+	data.rsvp.push(rsvpEmail);
+	//Send back data so we know it works
+	response.send(rsvpEmail);
+}
